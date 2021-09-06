aa35f80836c191dbbb2850a2a10ab344cd77433d
Francisco Salinas <remixielive@live.com>
2021-09-06 12:37:22 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+PATH = "/Users/rem/Documents/chromedriver"
+driver = webdriver.Chrome(PATH)
+driver.get('http://localhost:8888/arrow-dash/play.html')
+
+start_button = driver.find_element_by_id("start").click()
+
+time = driver.find_element_by_id("time").text
+
+global_page = driver.find_element_by_tag_name("body")
+
+arrow_array = [Keys.ARROW_LEFT, Keys.ARROW_UP, Keys.ARROW_RIGHT, Keys.ARROW_DOWN]
+
+while int(time) > 0:
+    current_arrow = (re.search("[0-3].*?",driver.find_elements_by_tag_name('img')[-1].get_attribute("src")))[0]
+    global_page.send_keys(arrow_array[int(current_arrow)])
---
+++ test2.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+PATH = "/Users/rem/Documents/chromedriver"
+driver = webdriver.Chrome(PATH)
+driver.get('https://forms.gle/1xKE9t5Mr75adPDDA')
+
