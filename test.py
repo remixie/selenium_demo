6aaed866d24f9ff1c0728608c12da182a4c38e84
Rem <remixielive@live.com>
2021-09-06 15:39:11 -0400

    Add second text

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-PATH = "/Users/rem/Documents/chromedriver"
+
+file = open("path.txt")
+PATH = file.read()
 driver = webdriver.Chrome(PATH)
 driver.get('http://localhost:8888/arrow-dash/play.html')
 
@@ -11,8 +13,10 @@ time = driver.find_element_by_id("time").text
 
 global_page = driver.find_element_by_tag_name("body")
 
-arrow_array = [Keys.ARROW_LEFT, Keys.ARROW_UP, Keys.ARROW_RIGHT, Keys.ARROW_DOWN]
+arrow_array = [Keys.ARROW_LEFT, Keys.ARROW_UP,
+               Keys.ARROW_RIGHT, Keys.ARROW_DOWN]
 
 while int(time) > 0:
-    current_arrow = (re.search("[0-3].*?",driver.find_elements_by_tag_name('img')[-1].get_attribute("src")))[0]
+    current_arrow = (re.search(
+        "[0-3].*?", driver.find_elements_by_tag_name('img')[-1].get_attribute("src")))[0]
     global_page.send_keys(arrow_array[int(current_arrow)])
--- test2.py
+++ test2.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.keys import Keys
-PATH = "/Users/rem/Documents/chromedriver"
+file = open("path.txt")
+PATH = file.read()
+
+print(PATH)
 driver = webdriver.Chrome(PATH)
 driver.get('https://forms.gle/1xKE9t5Mr75adPDDA')
+time.sleep(0.5)
 
+text_box = driver.find_elements_by_class_name(
+    "quantumWizTextinputPaperinputInput")
+text_box[0].send_keys("42")
+
+scale_selection = driver.find_elements_by_class_name(
+    "appsMaterialWizToggleRadiogroupOffRadio")[4].click()
+
+radio_selection = driver.find_elements_by_class_name(
+    "appsMaterialWizTogglePapercheckboxCheckbox")[0].click()
+
+text_box[1].send_keys("Nope. I am Selenium.")
+
+submit = driver.find_elements_by_class_name(
+    "appsMaterialWizButtonPaperbuttonContent")[0].click()
